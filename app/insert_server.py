88cc6436e8d1e82fa190473d650747c1642ad15c
Joey Hendriks <joeyhendriks@hotmail.com>
2019-06-06 12:36:13 +0200

    Inserted neat code for inserting into PromDB

---
+++ app/insert_server.py
@@ -0,0 +1,36 @@
+class PromInsertServer:
+    def init(self, port=8000):
+        """Start the http server for scraping
+        The port where you open should be scraped by prometheus
+        e.g port 8000 could have:
+        scrape_configs:
+          - job_name: 'local'
+            scrape_interval: 5s
+            static_configs:
+            - targets: ['localhost:8000']
+        """
+        start_http_server(port)
+        self.data = []
+        REGISTRY.register(self)
+
+    def collect(self):
+        """Is called when prometheus requests data from this server (it seems)
+        when multiple series with the same labels are updated, only takes the
+        latest one
+        """
+        for metric_name, value, labels, info, type_metric in self.data:
+            metric = Metric(metric_name, info, type_metric)
+            metric.add_sample(metric_name,
+                              value=float(value), labels=labels)
+            yield metric
+        self.data = []
+
+    def insert_into_prom(self, metric, value, labels, info="", type_metric='gauge'):
+        """metric_name is the name of actual metric, should be what the metric represents
+        value should either be a number (int, float) or string in the form of a number
+        labels should be a dict of the labels, with the keys the label names and values the label values
+        info is some info you want to add to the metric (can't find it in port 9090 though)
+        type_metric is the type of the metric, e.g. counter, gauge or histogram
+        """
+        labels = dict(metric, value)
+        self.data.append((metric, value, labels, info, type_metric))
---
+++ app/main.py
@@ -0,0 +1,28 @@
+from prometheus_client import start_http_server, Metric, REGISTRY
+import time
+from flask import Flask, request
+import json
+from insert_server import PromInsertServer
+
+app = Flask(__name__)
+
+###
+# [
+#   {
+#     metrics: { punctuality: '0' },
+#     meta: { operator_name: 'GVB', line_number: '22', vehicle_number: '362' }
+#   }
+# ]
+###
+@app.route('/', methods=['POST'])
+def main():
+    data = json.loads(request.get_json())
+    for data_point in data:
+        meta = data_points['meta']
+        metrics = data_points['metrics']
+        for metric_name, value in metrics:
+            ## TODO: Let API send metric type (gauge? counter?) and process
+            ## the metric type in this function.
+            server.insert_into_prom(metric_name, value, meta)
+
+server = PromInsertServer()
