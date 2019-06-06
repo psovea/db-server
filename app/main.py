40a5f7ec9802e674729bc2529b40d8660577c7ed
Joey Hendriks <joeyhendriks@hotmail.com>
2019-06-06 13:04:48 +0200

    fixed a last bug

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 def main():
     # print(request.get_json())
     data = request.get_json()
-    print("Got data: " + data)
+    print("Got data: " + str(data))
     for data_point in data:
         meta = data_point['meta']
         metrics = data_point['metrics']
