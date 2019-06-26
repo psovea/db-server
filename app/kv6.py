ef026c4c30a7c4d504f582870d12e415c6604034
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2019-06-26 12:43:04 +0200

    comments

--- app/kv6.py
+++ app/kv6.py
@@ -68,7 +68,7 @@ def get_stop_info(stop_code):
 
 
 def location_punctuality_metric(begin, end, increase, vehicle_number, line_number):
-    """Create a location punctuality metric from the given variables"""
+    """ Create a location punctuality metric from the given variables. """
     district = None
 
     try:
@@ -95,15 +95,17 @@ def location_punctuality_metric(begin, end, increase, vehicle_number, line_numbe
         }
     }
 
+
 def parse_arrivals(arrivals):
-    """ """
+    """ Parse the arrival messages from ndovloket such that, using the departure
+    info, the increase in punctuality for a vehicle can be calculated and
+    is added to the appropriate counter(s)."""
     for obj in arrivals:
         obj = obj[ARRIVAL]
         punc = int(obj['punctuality'])
         line_num = obj['lineplanningnumber']
         vehicle_num = obj['vehiclenumber']
 
-        prefix = None
         try:
             prefix = prefix_dict[obj['dataownercode']]
         except KeyError:
@@ -126,15 +128,17 @@ def parse_arrivals(arrivals):
         except KeyError:
             continue
 
+
 def parse_departures(departures):
-    """ """
+    """ Parse the departure messages from ndovloket such that the data in
+    tracking variables is correct for when the vehicle arrives at another
+    stop. """
     for obj in departures:
         obj = obj[DEPARTURE]
         punc = int(obj['punctuality'])
         line_num = obj['lineplanningnumber']
         vehicle_num = obj['vehiclenumber']
 
-        prefix = None
         try:
             prefix = prefix_dict[obj['dataownercode']]
         except KeyError:
@@ -164,9 +168,8 @@ def parse_departures(departures):
 
 
 def parse_message(message):
-    """ """
-    pos_info = None
-
+    """ Parse the message from ndovloket for arrivals and departures
+    for location_punctuality. """
     try:
         data = message['VV_TM_PUSH']['KV6posinfo']
         pos_info = [data] if type(data) is dict else data
@@ -180,13 +183,14 @@ def parse_message(message):
     parse_departures(departures)
 
 
-
 def ordered_dict_to_dict(od):
     """ hacky way to create regular dict from ordered dict. """
     return json.loads(json.dumps(od))
 
 
 if __name__ == '__main__':
+    """ If run as main, then set up a prometheus client and pass the inside
+    of the counters to be scraped. """
     from insert_server import PromInsertServer
     server = PromInsertServer(8001, counters)
     while True:
