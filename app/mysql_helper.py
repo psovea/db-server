f4f4edcb7309a05a86c475fffc703e5e5db39c4f
Joey Hendriks <joeyhendriks@hotmail.com>
2019-06-26 12:38:59 +0200

    added docs for mysql helper

--- app/mysql_helper.py
+++ app/mysql_helper.py
@@ -1,7 +1,9 @@
 import mysql.connector
 from environs import Env
 
-
+# Helper to build SELECT queries. Search_values is a dict that creates the
+# WHERE-clause. If split-search is True, the value of the dict needs to be a
+# list. This list gets put into an OR-clause within the WHERE-clause.
 def build_query(table, keys, search_values, inner_join=None, order_by=None, split_search=True):
     if split_search:
         search_params = " AND  ".join([
@@ -25,7 +27,8 @@ def build_query(table, keys, search_values, inner_join=None, order_by=None, spli
         query += "ORDER BY {}".format(",".join(order_by))
     return query
 
-
+# Class to manage MYSQL-connection and contains helpers for some often-used
+# queries. The constructor establishes the connection using the mysql-module
 class MysqlConnector:
     def __init__(self):
         env = Env()
@@ -42,6 +45,8 @@ class MysqlConnector:
         else:
             print("Can't connect to DB...")
 
+    # Executes a query. no-result needs to be true if the query is not
+    # a SELECT-query. only_one depicts if we only need one result.
     def execQuery(self, query, only_one=False, no_result=False):
         self.cursor.execute(query)
 
@@ -57,6 +62,8 @@ class MysqlConnector:
         res = self.cursor.fetchall()
         return res
 
+    # Gets the id of the record in table that complies to the search_values,
+    # which needs to be a dict.
     def getId(self, table, search_values):
         first_search_key, first_search_value = next(
             iter(search_values.items()))
@@ -71,6 +78,7 @@ class MysqlConnector:
             return id[0]
         return None
 
+    # Insert insert_values (dict) into table.
     def insert(self, table, insert_values):
         keys = ','.join(list(insert_values.keys()))
         values = ','.join(
@@ -80,12 +88,15 @@ class MysqlConnector:
         self.connection.commit()
         return self.cursor.lastrowid
 
+    # Gets the id of a record in table if a record matches search_values.
+    # Otherwise inserts insert_values
     def getOrInsert(self, table, search_values, insert_values):
         id = self.getId(table, search_values)
         if id:
             return id
         return self.insert(table, insert_values)
 
+    # Custom query that retrieves the transport_type of all lines.
     def getLineToTypeMapping(self):
         query = "SELECT transport_lines.public_id, transport_types.name FROM transport_lines LEFT JOIN transport_types ON transport_type_id = transport_types.id"
         self.cursor.execute(query)
